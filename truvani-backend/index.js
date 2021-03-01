76404f60b81947af9cbe9f0f32c17247a2d223b3
范淼 <femiajayi705@gmail.com>
2021-03-02 07:53:54 +0800

    feat: Added backend server with test route

---
+++ truvani-backend/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const port = process.env.PORT || 2888;
+
+const app = new express();
+app.use(cors());
+
+app.get("/test_server", (req, res) => {
+  res.send(
+    JSON.stringify({
+      truvani: "Server",
+      is: `ready to serve on port ${port}`,
+    })
+  );
+});
+
+app.listen(port, () => {
+  console.log(`Server started at localhost:${port}`);
+});
