391793632806ef1e8b9eabaad75099819247a0e0
范淼 <femiajayi705@gmail.com>
2021-03-02 09:43:57 +0800

    feat: Added front-end boiler plate files

---
+++ truvani-frontend/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Truvani full stack technical interview</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
