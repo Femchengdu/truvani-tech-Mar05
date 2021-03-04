54607c743ee12cde7af4d3226c91a1eb1c8c42ad
范淼 <femiajayi705@gmail.com>
2021-03-04 21:58:20 +0800

    chore: Added new reducers to the root reducer

--- truvani-frontend/src/redux/reducers/index.js
+++ truvani-frontend/src/redux/reducers/index.js
@@ -5,6 +5,8 @@ import errorReducer from "./errorReducer";
 import loadingReducer from "./loadingReducder";
 import productsReducer from "./productsReducer";
 import recommendationsReducer from "./recommendationsReducer";
+import recommenderErrorReducer from "./recommenderErrorReducer";
+import recommendationLoadingReducer from "./recommendationLoadingReducer";
 
 const rootReducer = combineReducers({
   cart: cartReducer,
@@ -12,6 +14,8 @@ const rootReducer = combineReducers({
   loading: loadingReducer,
   products: productsReducer,
   recommendations: recommendationsReducer,
+  recommenderErrro: recommenderErrorReducer,
+  recommendationLoading: recommendationLoadingReducer,
 });
 
 export default rootReducer;
