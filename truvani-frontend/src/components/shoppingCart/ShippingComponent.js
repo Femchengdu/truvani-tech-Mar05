68cf54a877f316aa225919c4b223c4953ced8c95
范淼 <femiajayi705@gmail.com>
2021-03-09 20:24:10 +0800

    bugfix: Show shippint text when no rewards qualify

--- truvani-frontend/src/components/shoppingCart/ShippingComponent.js
+++ truvani-frontend/src/components/shoppingCart/ShippingComponent.js
@@ -2,22 +2,20 @@ import React from "react";
 import { connect } from "react-redux";
 const ShippingComponent = ({ rewards }) => {
   let returnContent;
-  rewards === "Free Shipping and Totebag" ? (
-    (returnContent = (
-      <span>
-        Free Shipping <i class="fa fa-truck" aria-hidden="true"></i> + Tote bag{" "}
-        <i class="fa fa-shopping-bag" aria-hidden="true"></i>
-      </span>
-    ))
-  ) : rewards === "Free Shipping" ? (
-    (returnContent = (
-      <span>
-        Free Shipping <i class="fa fa-truck" aria-hidden="true"></i>
-      </span>
-    ))
-  ) : (
-    <span>Shipping Fee</span>
-  );
+  rewards === "Free Shipping and Totebag"
+    ? (returnContent = (
+        <span>
+          Free Shipping <i className="fa fa-truck" aria-hidden="true"></i> +
+          Tote bag <i className="fa fa-shopping-bag" aria-hidden="true"></i>
+        </span>
+      ))
+    : rewards === "Free Shipping"
+    ? (returnContent = (
+        <span>
+          Free Shipping <i className="fa fa-truck" aria-hidden="true"></i>
+        </span>
+      ))
+    : (returnContent = <span>Shipping Fee</span>);
 
   return <>{returnContent}</>;
 };
