785ccfde84fc0549945e9e9b7c1097b193ec1f1f
范淼 <femiajayi705@gmail.com>
2021-03-09 19:34:54 +0800

    feat: Added shipping rewards component

--- truvani-frontend/src/components/shoppingCart/CartSummary.js
+++ truvani-frontend/src/components/shoppingCart/CartSummary.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { connect } from "react-redux";
+import ShippingComponent from "./ShippingComponent";
 
-const CartSummary = ({ cartItems }) => {
+const CartSummary = ({ cartItems, rewards }) => {
   const cartItemsPriceSum = cartItems.reduce(
     (sum, item) => sum + item.price * item.qty,
     0
   );
   const taxPrice = cartItemsPriceSum * 0.1;
-  const shippingCost = cartItemsPriceSum > 500 ? 0 : 1.99;
+  const shippingCost =
+    rewards === "Free Shipping and Totebag"
+      ? 0
+      : rewards === "Free Shipping"
+      ? 0
+      : 1.99;
   const totalPrice = cartItemsPriceSum + shippingCost + taxPrice;
   return (
     <div>
@@ -25,7 +31,9 @@ const CartSummary = ({ cartItems }) => {
             <div className="col-xs-4 text-right">${taxPrice.toFixed(2)}</div>
           </div>
           <div className="row">
-            <div className="col-xs-8">Shipping Price</div>
+            <div className="col-xs-8">
+              <ShippingComponent />
+            </div>
             <div className="col-xs-4 text-right">
               ${shippingCost.toFixed(2)}
             </div>
@@ -44,8 +52,9 @@ const CartSummary = ({ cartItems }) => {
   );
 };
 
-const mapStateToProps = ({ cartItems }) => ({
+const mapStateToProps = ({ cartItems, rewards }) => ({
   cartItems,
+  rewards,
 });
 
 export default connect(mapStateToProps)(CartSummary);
---
+++ truvani-frontend/src/components/shoppingCart/ShippingComponent.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { connect } from "react-redux";
+const ShippingComponent = ({ rewards }) => {
+  let returnContent;
+  rewards === "Free Shipping and Totebag" ? (
+    (returnContent = (
+      <span>
+        Free Shipping <i class="fa fa-truck" aria-hidden="true"></i> + Totebag{" "}
+        <i class="fa fa-shopping-bag" aria-hidden="true"></i>
+      </span>
+    ))
+  ) : rewards === "Free Shipping" ? (
+    (returnContent = (
+      <span>
+        Free Shipping <i class="fa fa-truck" aria-hidden="true"></i>
+      </span>
+    ))
+  ) : (
+    <span>Shipping Fee</span>
+  );
+
+  return <>{returnContent}</>;
+};
+
+const mapStateToProps = ({ rewards }) => ({
+  rewards,
+});
+
+export default connect(mapStateToProps)(ShippingComponent);
